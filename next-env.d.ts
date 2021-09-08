e7fd782e37c37e2062b99c350985ae2c65ebb825
yilinng <31823305+yilinng@users.noreply.github.com>
2021-09-08 20:31:51 +0800

    Update next-env.d.ts
    https://github.com/nextauthjs/next-auth/issues/824

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,15 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
+declare global {
+    namespace NodeJS {
+        interface Global {
+            prisma: any;
+        }
+    }
+}
+
+export { }
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
