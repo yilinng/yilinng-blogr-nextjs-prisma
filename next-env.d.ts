f1fd21c29bb3d5314d5b0566c479021ab80d1ed1
yilinng <31823305+yilinng@users.noreply.github.com>
2021-09-08 21:31:59 +0800

    Update next-env.d.ts
    remove global namespace

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,15 +2,7 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-declare global {
-    namespace NodeJS {
-        interface Global {
-            prisma: any;
-        }
-    }
-}
 
-export { }
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
