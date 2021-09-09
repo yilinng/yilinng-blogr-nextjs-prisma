93fe89c702ced661f310884d12ebc0702ab9d131
yilinng <31823305+yilinng@users.noreply.github.com>
2021-09-09 14:20:37 +0800

    Update index.tsx
    import server

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { GetStaticProps } from "next"
+import { GetServerSideProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma from '../lib/prisma';
