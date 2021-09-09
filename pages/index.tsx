7e0e0799236710eabdbf11a2d4a8469bac8ef2d4
yilinng <31823305+yilinng@users.noreply.github.com>
2021-09-09 14:18:05 +0800

    Update index.tsx
    change to server render

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
 import prisma from '../lib/prisma';
 
-export const getStaticProps: GetStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
     include: {
